5475084ef27a36d8fd10026ab56bd7a08f93b48c
Nebye <nebyeb@hotmail.com>
2021-01-17 11:38:15 -0700

    A little bit better?

---
+++ Call it a day/Assets/Scripts/bottom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class bottom : MonoBehaviour
+{
+ 
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                //Pickup();
+                other.GetComponent<Rigidbody2D>().mass = .8f;
+                SceneManager.LoadScene(sceneName: "Opening");
+            }
+        }
+    
+}
