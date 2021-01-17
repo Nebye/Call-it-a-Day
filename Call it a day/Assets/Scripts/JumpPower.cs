7fac25120a3cd561b1615d604876bf5cab542c59
Kevin <nguyen.kev98@gmail.com>
2021-01-16 22:29:54 -0700

    Jump Power

---
+++ Call it a day/Assets/Scripts/JumpPower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Pickup();
+            other.GetComponent<Rigidbody2D>().mass = .8f;
+            Destroy(gameObject);
+        }
+    }
+    
+
+    
+}
