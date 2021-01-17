1442607590172e199a4ef91ace0ff0b849a14a59
Nebye <nebyeb@hotmail.com>
2021-01-16 21:34:54 -0700

    In Unity Camera Follows You

---
+++ Call it a day/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+    public float cameraDis;
+
+    public GameObject Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = (Screen.height / 2) / cameraDis;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+}
