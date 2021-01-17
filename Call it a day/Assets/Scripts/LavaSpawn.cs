45240591a3894d24c333402b70ea418b06579456
Nebye <nebyeb@hotmail.com>
2021-01-17 07:14:17 -0700

    Auto stash before merge of "main" and "origin/main"

---
+++ Call it a day/Assets/Scripts/LavaSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class LavaSpawn : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frameArray;
+    private int currentFrame;
+    private float timer;
+
+    private void Update()
+    {
+        timer = timer + Time.deltaTime;
+
+        if (timer>= 1f)
+        {
+            timer = timer - 0.10f;
+            currentFrame = (currentFrame + 1) % frameArray.Length;
+            gameObject.GetComponent<SpriteRenderer>().sprite = frameArray[currentFrame];
+        }
+    }
+
+    // if level starts trigger co-routine
+    public void StartLavaFlood()
+    {
+
+    }
+
+}
